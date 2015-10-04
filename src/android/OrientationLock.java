07ae3a63794c27dcee79fa1c1ba2e1f01b2b0378
Robert Hill <dr.pappa@gmail.com>
2015-10-04 16:59:02 +0100

    Initial commit, not tested

---
+++ src/android/OrientationLock.java
@@ -0,0 +1,32 @@
+package com.DungBeetleBASH.OrientationLock;
+
+import org.apache.cordova.CordovaPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+public class OrientationLock extends CordovaPlugin {
+
+  public boolean execute(String action, JSONArray args) throws JSONException {
+    if (action.equals("setOrientation")) {
+      this.setOrientation(args.getString(0));
+      return true;
+    }
+    return false;
+  }
+
+  private void setOrientation(orientation) {
+    Activity activity = cordova.getActivity();
+    if (orientation.equalsIgnoreCase("landscape")) {
+      activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    } else if (orientation.equalsIgnoreCase("portrait")) {
+      activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    } else {
+      activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    } 
+  }
+
+}
---
+++ www/orientationLock.js
@@ -0,0 +1,11 @@
+var exec = require('cordova/exec');
+
+/**
+ * Provides access to the various notification features on the device.
+ */
+
+module.exports = {
+	setOrientation: function(orientation){
+		exec(null, null, "OrientationLock", "setOrientation", [orientation]);
+	}
+};
